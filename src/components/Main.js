e108a4fe96c585fd256d68003cf5eefc81f5a831
bearkuang <137677440+bearkuang@users.noreply.github.com>
2023-11-07 18:32:26 +0900

    메인화면 틀

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import Slider from 'react-slick';
 import "./Main.css";
 import Layout from './LayoutGrid';
 import Searchbar from './Fix/Searchbar';
-import Skilldropdown from './Fix/Skilldropdown';
+import Skilldropdown from './Fix/SkilldropdownList';
+import Checkbox from "./Fix/Checkbox";
 
 const Main = () => {
     var settings = {
@@ -48,12 +49,18 @@ const Main = () => {
                         </Slider>
                     </div><br /><br /><br />
                 </section>
-                <div className="contents">
-            <div className="grid grid-rows-1 grid-flow-col">
-                        <Skilldropdown className="inline-block"></Skilldropdown>
-                        <Searchbar className="inline-block right:500px"></Searchbar>
+                <div className="fixed-container">
+                    <div className="grid grid-rows-1 grid-cols-7">
+                        <div><button className="custom-button text-2xl text-black"><h1>전체보기</h1></button></div>
+                        <div><button className="custom-button text-2xl text-black"><h1>백엔드</h1></button></div>
+                        <div><button className="custom-button text-2xl text-black"><h1>프론드엔드</h1></button></div>
+                        <div><Skilldropdown className="inline-block text-2xl text-black"></Skilldropdown></div>
+                        <div></div>
+                        <div><Checkbox /></div>
+                        <div><Searchbar className="inline-block"></Searchbar></div>
+                        <div></div>
                     </div>
-            </div>
+                </div>
             </Layout>
         </>
     );
