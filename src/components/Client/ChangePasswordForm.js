84505ee1e9b111ed6d898fd0ae5e4aabe789f023
Chan <chc6823@naver.com>
2023-11-08 15:48:49 +0900

    asd

---
+++ src/components/Client/ChangePasswordForm.js
@@ -0,0 +1,35 @@
+import "/work_react/Recode2/src/components/Client/ChangePasswordForm.css";
+
+const ChangePasswordForm = () => {
+  return (
+    <div className="div">
+      <div className="div1">
+        <img className="child" alt="" src="/Rectangle48.png" />
+        <div className="item" />
+        <div className="parent">
+          <div className="div2">새 비밀번호</div>
+          <div className="frame-child" />
+          <div className="div3">**********</div>
+          <div className="div4">비밀번호 확인</div>
+          <div className="frame-item" />
+          <div className="div5">**********</div>
+          <div className="frame-inner" />
+          <div className="div6">비밀번호 표시</div>
+        </div>
+        <div className="div7">
+          안전한 비밀번호로 계정을 보호하세요. 다른 곳에서 사용하지 않는
+          비밀번호 설정을 권한합니다.
+        </div>
+        <img className="inner" alt="" src="/Rectangle52.png" />
+        <div className="div8">비밀번호 변경</div>
+      </div>
+      <div className="group">
+        <div className="div9">비밀번호 변경</div>
+        <img className="right-arrow-1-icon" alt="" src="/right-arrow1.png" />
+      </div>
+      <div className="line-div" />
+    </div>
+  );
+};
+
+export default ChangePasswordForm;
