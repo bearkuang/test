19128fa52c43098d8419a3eeafeb56b3f3835089
bearkuang <137677440+bearkuang@users.noreply.github.com>
2023-11-08 17:49:21 +0900

    헤더 변경

--- src/components/Mypage/Mypage_Myprofile.js
+++ src/components/Mypage/Mypage_Myprofile.js
@@ -1,9 +1,171 @@
 import React from "react";
 import MypageSidebar from "./MypageSidebar";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { PencilIcon } from "@heroicons/react/24/solid";
+import {
+    Card,
+    CardHeader,
+    Typography,
+    CardBody,
+    Chip,
+    Avatar,
+    IconButton,
+    Tooltip,
+} from "@material-tailwind/react";
+
+
+const TABLE_ROWS = [
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
+        name: "John Michael",
+        email: "john@creative-tim.com",
+        job: "Manager",
+        org: "Organization",
+        online: true,
+        date: "23/04/18",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
+        name: "Alexa Liras",
+        email: "alexa@creative-tim.com",
+        job: "Programator",
+        org: "Developer",
+        online: false,
+        date: "23/04/18",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
+        name: "Laurent Perrier",
+        email: "laurent@creative-tim.com",
+        job: "Executive",
+        org: "Projects",
+        online: false,
+        date: "19/09/17",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
+        name: "Michael Levi",
+        email: "michael@creative-tim.com",
+        job: "Programator",
+        org: "Developer",
+        online: true,
+        date: "24/12/08",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
+        name: "Richard Gran",
+        email: "richard@creative-tim.com",
+        job: "Manager",
+        org: "Executive",
+        online: false,
+        date: "04/10/21",
+    },
+];
+
 
 const Mypage_Myprofile = () => {
     return (
-        <MypageSidebar></MypageSidebar>
+        <>
+            <MypageSidebar />
+            <Card className="h-full w-auto">
+                <CardHeader floated={false} shadow={false} className="rounded-none">
+                    <div className="mb-8 flex items-center justify-between gap-8">
+                        <div>
+                            <Typography variant="h5" color="blue-gray">
+                                개인 정보
+                            </Typography>
+                            <Typography color="gray" className="mt-1 font-normal">
+                                일부 정보가 서비스를 사용하는 다른 사용자에게 표시될 수 있습니다
+                            </Typography>
+                        </div>
+                    </div>
+                </CardHeader>
+                <CardBody className="overflow-scroll px-0">
+                    <table className="mt-4 w-full min-w-max table-auto text-left">
+                        <tbody>
+                            {TABLE_ROWS.map(
+                                ({ img, name, email, job, org, online, date }, index) => {
+                                    const isLast = index === TABLE_ROWS.length - 1;
+                                    const classes = isLast
+                                        ? "p-4"
+                                        : "p-4 border-b border-blue-gray-50";
+
+                                    return (
+                                        <tr key={name}>
+                                            <td className={classes}>
+                                                <div className="flex items-center gap-3">
+                                                    <Avatar src={img} alt={name} size="sm" />
+                                                    <div className="flex flex-col">
+                                                        <Typography
+                                                            variant="small"
+                                                            color="blue-gray"
+                                                            className="font-normal"
+                                                        >
+                                                            {name}
+                                                        </Typography>
+                                                        <Typography
+                                                            variant="small"
+                                                            color="blue-gray"
+                                                            className="font-normal opacity-70"
+                                                        >
+                                                            {email}
+                                                        </Typography>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className={classes}>
+                                                <div className="flex flex-col">
+                                                    <Typography
+                                                        variant="small"
+                                                        color="blue-gray"
+                                                        className="font-normal"
+                                                    >
+                                                        {job}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="small"
+                                                        color="blue-gray"
+                                                        className="font-normal opacity-70"
+                                                    >
+                                                        {org}
+                                                    </Typography>
+                                                </div>
+                                            </td>
+                                            <td className={classes}>
+                                                <div className="w-max">
+                                                    <Chip
+                                                        variant="ghost"
+                                                        size="sm"
+                                                        value={online ? "online" : "offline"}
+                                                        color={online ? "green" : "blue-gray"}
+                                                    />
+                                                </div>
+                                            </td>
+                                            <td className={classes}>
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal"
+                                                >
+                                                    {date}
+                                                </Typography>
+                                            </td>
+                                            <td className={classes}>
+                                                <Tooltip content="Edit User">
+                                                    <IconButton variant="text">
+                                                        <PencilIcon className="h-4 w-4" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </td>
+                                        </tr>
+                                    );
+                                },
+                            )}
+                        </tbody>
+                    </table>
+                </CardBody>
+            </Card>
+        </>
     );
-    }
+}
 export default Mypage_Myprofile;
